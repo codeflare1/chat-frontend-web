912abbb562bd551934ac602a4e6e7de1d1ccd39b
codeflare1 <codeflare140324@gmail.com>
2024-10-09 09:51:56 +0530

    content update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,15 +9,15 @@ function HeroSection() {
                 <div className="container mx-auto">
                     <div className="flex flex-col md:flex-row gap-6 items-center">
                         <div className="content md:text-left">
-                            <h1 className='text-4xl md:text-64 font-bold text-white'>
-                            Gatsbychat, an Connection and Commerce App
+                            <h1 className='text-4xl md:text-64 md:leading-snug font-bold text-white'>
+                            Gatsbychat, where Privacy seamlessly meets Connection.
                             </h1>
                             <p className='text-sm md:text-xl text-white leading-loose mb-30'>
-                                Welcome to Gatsbychat, a revolutionary platform for social networking, instant messaging, e-commerce, and real-time communication. We empower users to connect, share, and thrive in a vibrant digital ecosystem.
+                            A revolutionary platform for social networking, messaging, and meetings, empowering users to connect, share, and thrive in a secure digital ecosystem that prioritizes privacy and real-time communication.
                             </p>
                             <Stack spacing={2} direction="row" className='btn flex gap-4 md:gap-2 items-center mb-30 justify-center md:justify-start'>
                                 <Button variant="contained" className='px-50 py-5 bg-white rounded-full text-Newblack font-semibold text-lg leading-4 border border-white'>Download</Button>
-                                <Button variant="text" className='px-50 py-5 text-white underline underline-offset-2 font-semibold text-lg rounded-full' >Learn more</Button>
+                                <Button href='/login' variant="text" className='px-50 py-5 text-white underline underline-offset-2 font-semibold text-lg rounded-full h-14' >Login</Button>
                             </Stack>
                             <div className="App__download flex justify-center md:justify-start items-center gap-2.5">
                                 <a href="/">
